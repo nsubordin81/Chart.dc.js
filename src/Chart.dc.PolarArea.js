91fa9cd3d2d6099db0b8126aa2f6fdfd81c7f44c
Taylor Bird <nsubordin81@gmail.com>
2015-09-09 14:57:47 -0400

    updated header comment in source and fixed case issue in example

--- src/Chart.dc.PolarArea.js
+++ src/Chart.dc.PolarArea.js
@@ -1,3 +1,29 @@
+/* 
+*   Chart.dc.js v. 0.1.0
+*
+*   The MIT License (MIT)
+*
+*   Copyright (c) 2015 Taylor Bird
+*   
+*   Permission is hereby granted, free of charge, to any person obtaining a copy
+*   of this software and associated documentation files (the "Software"), to deal
+*   in the Software without restriction, including without limitation the rights
+*   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*   copies of the Software, and to permit persons to whom the Software is
+*   furnished to do so, subject to the following conditions:
+*   
+*   The above copyright notice and this permission notice shall be included in
+*   all copies or substantial portions of the Software.
+*   
+*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+*   THE SOFTWARE.   
+*
+*/
 (function() {
     "use strict";
 
@@ -125,7 +151,7 @@
             var argsArray = Array.prototype.slice.call(arguments);
             //remove the first element of arguments which is our array, then we do a bunch of Chartjs converison on it . . . 
             argsArray.splice(0, 1);
-            //TODO - check if data is an array, if not, put a message in a console explaining how you are supposed to send 
+            //TODO - check if data is an array, if not, put a message in a console explaining how you are supposed to send data in an array 
             this.dimension = data.dimension;
             data.chartGroup ? this.chartGroup = data.chartGroup : this.chartGroup = 0;
             //short but magical line. Now we are linked with all dc charts in this group!
